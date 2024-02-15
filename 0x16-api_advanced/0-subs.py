4183b6952bc93e0f3517f2f5bf78d7345174055a
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-15 17:01:43 +0100

    0x18-webstack_monitoring

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""Queries the `Reddit API`, returns the number of subscribers
-    of a subreddit
-"""
+"""Function to query subscribers (total subscribers) for a given subreddit"""
 import requests
 
-
+# Function definition to get the number of subscribers for a given subreddit
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {"User-Agent": "Mozilla/5.0"}
-
-    data = requests.get(url, headers=header, allow_redirects=False)
-
-    if data.status_code == 200:
-        subscribers = data.json().get("data").get("subscribers")
-        return subscribers
+    """Return the total number of subscribers on a given subreddit."""
+    
+    # Construct the URL to retrieve subreddit information in JSON format
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    # Define headers for the HTTP request, including a User-Agent
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/tonyrealzy)"
+    }
+    
+    # Send an HTTP GET request to the specified URL with the provided headers
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the response status code indicates success
+    if response.status_code == 200:      
+        # Parse the JSON response and extract the "data" section
+        data = response.json().get("data")
+        
+        # Return the number of subscribers from the "subscribers" key in the "data" section
+        return data.get("subscribers")
+    
     return 0
