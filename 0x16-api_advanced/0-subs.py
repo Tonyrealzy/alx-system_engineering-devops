5d3b6ecdab9f2d3fe770f6d6b25b330b824e7308
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-16 12:11:21 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,21 +6,19 @@ import requests
 # Function definition to get the number of subscribers for a given subreddit
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    
     # Construct the URL to retrieve subreddit information in JSON format
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     # Define headers for the HTTP request, including a User-Agent
     header = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/tonyrealzy)"
     }
-    
     # Send an HTTP GET request to the specified URL with the provided headers
     data = requests.get(url, headers=header, allow_redirects=False)
-    
     # Check if the response status code indicates success
     if data.status_code == 200:      
         # Parse the JSON response and extract the "data" section
         subscribers = data.json().get("data").get("subscribers")
-        # Return the number of subscribers from the "subscribers" key in the "data" section
+        # Return the number of subscribers from the "subscribers" key in
+        # the "data" section
         return subscribers
     return 0
